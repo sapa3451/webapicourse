176f298248b2a616b8d24e42d92c984ad0bd90f7
Salvatore Pacifico <sapa3451@colorado.edu>
2022-11-14 16:32:57 -0500

    done

---
+++ ApiBasicsSolution/ApiBasics/Controllers/StatusController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBasics.Controllers
+{
+    public class StatusController: ControllerBase
+    {
+        [HttpGet("/status")]
+        public ActionResult GetTheStatus()
+        {
+            return Ok("Everything looks good");
+        }
+    }
+}
