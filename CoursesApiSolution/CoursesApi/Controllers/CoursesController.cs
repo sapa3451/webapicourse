d530d2076e8f975935ecece019f37a6086153aa8
Salvatore Pacifico <sapa3451@colorado.edu>
2022-11-15 15:56:33 -0500

    done 2

---
+++ CoursesApiSolution/CoursesApi/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursesApi.Controllers;
+
+public class CoursesController : ControllerBase
+{
+
+    private readonly CourseCatalog _catalog;
+
+    public CoursesController(CourseCatalog catalog)
+    {
+        _catalog = catalog;
+    }
+
+    [HttpGet("/courses")]
+    public async Task<ActionResult> GetCoursesAsync()
+    {
+        CoursesResponseModel response = await _catalog.GetFullCatalogAsync();
+
+        //var response = new CoursesResponseModel { NumberOfBackendCourses = 99, NumberOfFrontendCourses = 12 };
+        return Ok(response);
+    }
+}
---
+++ CoursesApiSolution/CoursesApi/Domain/CourseCatalog.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursesApi.Domain;
+
+public class CourseCatalog
+{
+    private readonly CoursesDataContext _context;
+
+    public CourseCatalog(CoursesDataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CoursesResponseModel> GetFullCatalogAsync()
+    {
+        var courses = await _context.Courses.Where(c => c.Retired == false)
+            .Select(c => new CourseItemResponse
+            {
+                Id = c.Id.ToString(),
+                Title = c.Title,
+                Category = c.Category
+            }).ToListAsync();
+
+        return new CoursesResponseModel
+        {
+            Courses = courses,
+            NumberOfBackendCourses = courses.Count(c => c.Category == CategoryType.Backend),
+            NumberOfFrontendCourses = courses.Count(c => c.Category == CategoryType.Frontend)
+        };
+    }
+}
